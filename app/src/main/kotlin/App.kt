ffc5576e1e68d8aef464aa21b4c442ba4fc664df
JunseokOh <ixtears23@icloud.com>
2024-11-21 01:19:06 +0900

    프로젝트 생성

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package junseok.snr.kotlin.app
+
+import junseok.snr.kotlin.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
